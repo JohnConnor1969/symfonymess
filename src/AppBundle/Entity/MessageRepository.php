f9e2451f48aad9c16817cce6bbc6a6dd80520366
john conor <conor.john.1969@gmail.com>
2016-03-03 18:13:32 +0600

    early Query

--- src/AppBundle/Entity/MessageRepository.php
+++ src/AppBundle/Entity/MessageRepository.php
@@ -6,5 +6,17 @@ use Doctrine\ORM\EntityRepository;
 
 class MessageRepository extends EntityRepository
 {
+    /**
+     * @param Device $device
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function getMessagesForDevice(Device $device)
+    {
+        $now = date('Y-m-d');
+
+        return $this->createQueryBuilder('b')
+            ->
+            ->getQuery()->execute();
+    }
 
 }
