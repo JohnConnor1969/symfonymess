88b736fcd17c11df19f20c9073f83fb384bfbb1e
john conor <conor.john.1969@gmail.com>
2016-03-03 19:05:14 +0600

    something Query

--- src/AppBundle/Entity/MessageRepository.php
+++ src/AppBundle/Entity/MessageRepository.php
@@ -13,9 +13,15 @@ class MessageRepository extends EntityRepository
     public function getMessagesForDevice(Device $device)
     {
         $now = date('Y-m-d');
+        $begin = date_create_from_format("Y-m-d H:i:s", $now . " 00:00:00");
+        $end = date_create_from_format("Y-m-d H:i:s", $now . " 23:59:59");
 
         return $this->createQueryBuilder('b')
-            ->
+            ->andWhere('b.date > :now')
+            ->join('AppBundle:Device', 'devi', 'WITH', 'b.device = devi.id')
+            ->setParameter('begin', $begin)
+            ->setParameter('end', $end)
+            ->setParameter('pid', $device)
             ->getQuery()->execute();
     }
 
