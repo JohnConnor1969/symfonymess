8ac314a14429b9f41022098aeed74260196b4f40
john conor <conor.john.1969@gmail.com>
2016-03-03 10:30:19 +0600

    early sketch2

--- src/AppBundle/Entity/Device.php
+++ src/AppBundle/Entity/Device.php
@@ -2,7 +2,118 @@
 /**
  * Created by PhpStorm.
  * User: Admin
- * Date: 3/2/2016
- * Time: 6:54 PM
+ * Date: 3/3/2016
+ * Time: 9:45 AM
  */
 
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * Class Device
+ *
+ * @ORM\table(name="Device")
+ * @ORM\Entity
+ */
+class Device
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="device_id", type="integer")
+     */
+    private $device_id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="group", type="string", length=255 )
+     */
+    private $group;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="wiewedmessage", type="string", length=255)
+     */
+    private $viewedmessage; //TODO "in plural"
+
+    public function __construct()
+    {
+        $this->createdAt = time();
+        $this->device = new ArrayCollection();
+    }
+
+    public function __toString()
+    {
+        return (string) $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDeviceId()
+    {
+        return $this->device_id;
+    }
+
+    /**
+     * @param int $device_id
+     */
+    public function setDeviceId($device_id)
+    {
+        $this->device_id = $device_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGroup()
+    {
+        return $this->group;
+    }
+
+    /**
+     * @param string $group
+     */
+    public function setGroup($group)
+    {
+        $this->group = $group;
+    }
+
+    /**
+     * @return string
+     */
+    public function getViewedmessage()
+    {
+        return $this->viewedmessage;
+    }
+
+    /**
+     * @param string $viewedmessage
+     */
+    public function setViewedmessage($viewedmessage)
+    {
+        $this->viewedmessage = $viewedmessage;
+    }
+
+
+}
--- src/AppBundle/Entity/Message.php
+++ src/AppBundle/Entity/Message.php
@@ -12,7 +12,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Channel
+ * Message
  *
  * @ORM\Table(name="message")
  * @ORM\Entity
@@ -127,5 +127,87 @@ class Message {
     {
         return $this->name;
     }
+
+    /**
+     * @return string
+     */
+    public function getGroup()
+    {
+        return $this->group;
+    }
+
+    /**
+     * @param string $group
+     */
+    public function setGroup($group)
+    {
+        $this->group = $group;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDevice()
+    {
+        return $this->device;
+    }
+
+    /**
+     * @param string $device
+     */
+    public function setDevice($device)
+    {
+        $this->device = $device;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return int
+     */
+    public function getExpiration()
+    {
+        return $this->expiration;
+    }
+
+    /**
+     * @param int $expiration
+     */
+    public function setExpiration($expiration)
+    {
+        $this->expiration = $expiration;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCondition()
+    {
+        return $this->condition;
+    }
+
+    /**
+     * @param string $condition
+     */
+    public function setCondition($condition)
+    {
+        $this->condition = $condition;
+    }
+
+
 
 }
