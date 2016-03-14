9540b9c1a39310b9a6bc48a91d1f1d395d5fe822
john conor <conor.john.1969@gmail.com>
2016-03-14 16:19:43 +0600

    write repository method

--- src/AppBundle/Entity/MessageRepository.php
+++ src/AppBundle/Entity/MessageRepository.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Query\AST\Functions;
 
 class MessageRepository extends EntityRepository
 {
@@ -14,32 +13,29 @@ class MessageRepository extends EntityRepository
     public function getMessagesForDevice(Device $device)
     {
         $now = date('Y-m-d');
-        $begin = date_create_from_format("Y-m-d H:i:s", $now . " 00:00:00");
-        $end = date_create_from_format("Y-m-d H:i:s", $now . " 23:59:59");
 
-//        return $this->createQueryBuilder('b')
-//            ->andWhere(':now > :begin AND :now < :end')
-//            ->join('AppBundle:Device', 'devi', 'WITH', 'b.device = devi.id')
-//            ->setParameter('begin', $begin)
-//            ->setParameter('end', $end)
-//            ->setParameter('did', $device)
-//            ->setParameter('now', $now)
-//            ->getQuery()->execute();
-
-
-        $sql = $this->createQueryBuilder('b' );
-
+        return $this->createQueryBuilder('n')
+            ->andWhere("DATE_ADD(n.targetDate, n.expiration, 'day') < :now" AND "n.targetDate > :now")
+            ->andWhere(":dev.groupOf == n.targetGroup" || ":dev.uniqueId == n.targetDevice")
+            ->andWhere("n.informedDevices != :dev")
+            ->setParameter('now', $now)
+            ->setParameter('dev', $device)
+            ->getQuery()->execute();
 
     }
+
     /**
      * @return \Doctrine\ORM\QueryBuilder
      */
-    public function getMessagesNotExpirated ()
+    public function getActiveMessages ()
     {
-//        $now = date('Y-m-d');
+        $now = date('Y-m-d');
 
-//        return $this->createQueryBuilder('n')
-//            ->andWhere(DATE_ADD(n.targetDate, n.expiration, "day" ));
+        return $this->createQueryBuilder('n')
+            ->andWhere("DATE_ADD(n.targetDate, n.expiration, 'day') < :now" AND "n.targetDate > :now")
+            ->setParameter('now', $now)
+            ->getQuery()->execute();
+
     }
 
 }
